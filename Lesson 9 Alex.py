b7b10b16867f6d1fc4b238074d7feffbf2911a43
sid <ya@ya.ru>
2022-10-05 09:32:53 +0300

    update

--- Lesson 9 Alex.py
+++ Lesson 9 Alex.py
@@ -50,13 +50,17 @@
 #     s //= 2
 # print(chislo)
 
-s = input()
-print(s[2], s[-2], s[0:5], s[0:-2], s[0::2], s[1::2], s[::-1], s[::-2], sep='\n')
+# s = input()
+# print(s[2], s[-2], s[0:5], s[0:-2], s[0::2], s[1::2], s[::-1], s[::-2], sep='\n')
+
+# s = input()
+# pol = int((len(s) + 1) / 2)
+# print(s[pol:] + s[:pol])
 
 
-
-
-
+s = 'one'
+print(s[0])
+print(s[1])
 
 
 
