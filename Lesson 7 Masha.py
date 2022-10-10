70f724d8a9ae89cda0702c44c06629967b169350
Masha <m.s.galitsina@yandex.ru>
2022-10-10 20:39:57 +0300

    hello world

---
+++ Lesson 7 Masha.py
@@ -0,0 +1,9 @@
+# a1, b1, a2, b2 = int(input()), int(input()), int(input()), int(input())
+# if a2 <= a1 <= b2:
+#     print(a1)
+# else:
+#     if a2 <= b1 <= b2:
+#         print(b1)
+
+print("Hello world")
+
